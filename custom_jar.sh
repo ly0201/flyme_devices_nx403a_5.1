653dae3ac4834e7fb8d681756553e13d2b458f3a
ly0201 <529527001@qq.com>
2015-10-25 21:06:41 +0800

    flyme app

---
+++ custom_jar.sh
@@ -0,0 +1,12 @@
+
+jarBaseName=$1
+tempSmaliDir=$2
+
+dir1=android/accounts/
+dir2=android/widget/
+
+if [ "$jarBaseName" = "framework" ];then
+	echo ">>> in custom_jar $jarBaseName"
+        mv $tempSmaliDir/smali/$dir1 $tempSmaliDir/smali_classes2/$dir1
+        mv $tempSmaliDir/smali/$dir2 $tempSmaliDir/smali_classes2/$dir2
+fi
